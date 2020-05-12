c67ab7b93df5a4ace7c38550440815ab0879aeed
curdy <rhulani.ndhlvou1995@gmail.com>
2020-05-12 22:33:25 +0200

    Initial Commit

---
+++ signarfy001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  signarfy001
+//
+//  Created by SBI Admin on 2020/05/12.
+//  Copyright © 2020 Rhulani ndhlovu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ signarfy001Tests/signarfy001Tests.swift
@@ -0,0 +1,34 @@
+//
+//  signarfy001Tests.swift
+//  signarfy001Tests
+//
+//  Created by SBI Admin on 2020/05/12.
+//  Copyright © 2020 Rhulani ndhlovu. All rights reserved.
+//
+
+import XCTest
+@testable import signarfy001
+
+class signarfy001Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
